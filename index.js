9b75cf8b7426e7b39b2da93502f1364409ce0e50
tobias <tobias.omondi@student.moringaschool.com>
2024-05-20 12:31:38 +0300

    basic js

---
+++ index.js
@@ -0,0 +1,24 @@
+// var number = 52345;
+// var number2 = 5;
+//     result = number + number2;
+
+
+var name = 'Tobias'; // global variable
+console.log (name)
+
+var number1 =10,
+    number3 = 22,
+    result;
+    
+if (typeof result === "undefined") {
+    result = number1 + number3;
+}
+// The typeof operator is particularly useful for checking the data type of variables, ensuring type safety, and debugging.
+
+var number4 = 'a',
+    number5 = 22,
+    result = 'Tobias';
+    
+if ( isNaN  (result / 2)) {
+    console.log ('not number')
+}
